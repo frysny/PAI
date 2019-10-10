005f6c1001cd3fdb2898933010580ab91cfe7983
frysny <kfrysny@gmail.com>
2019-10-10 11:33:31 +0200

    start

---
+++ PAI_frysny/js/Rejestracja.js
@@ -0,0 +1,52 @@
+﻿function sprawdz(form) {
+    var user = form.nazwa.value;
+    if ((user.length < 5) || (user.length > 10)) {
+        document.getElementById("zlaNazwa").innerHTML = "Nazwa użytkownika może mieć od 5 do 10 liter";
+        document.getElementById("zlaNazwa").className = "wyroznienieCzerwone";
+        form.nazwa.focus;
+        return false;
+    }   
+    else { document.getElementById("zlaNazwa").innerHTML = ""; }
+    if (form.haslo.value.length < 7) {
+        document.getElementById("opisHasla").innerHTML = "Proszę wprowadzić hasło co najmniej 7 literowe.";
+        document.getElementById("opisHasla").className = "wyroznienieCzerwone";
+        form.haslo.focus();
+        return false;
+    }
+    if (!((user.match(/[a-z]/)) || (user.match(/[A-Z]/)))) {
+        document.getElementById("zlaNazwa").innerHTML = "Nazwa użytkownika ma zawietać tylko litery";
+        document.getElementById("zlaNazwa").className = "wyroznienieCzerwone";
+        form.nazwa.focus;
+        return false;
+    }
+    else { document.getElementById("zlaNazwa").innerHTML = ""; }
+    if (form.haslo.value != form.haslo2.value) {
+        document.getElementById("zgodneHasla").innerHTML = "Hasla nie są zgodne!";
+        document.getElementById("zgodneHasla").className = "wyroznienieCzerwone";
+        return false;
+    }
+    return true;
+}
+function silaHasla(haslo) {
+    var punkty = 0;
+    if (haslo.length >= 7) punkty++;
+    if ((haslo.match(/[a-z]/)) && (haslo.match(/[A-Z]/))) punkty++;
+    if (haslo.match(/\d+/)) punkty++;
+    if (haslo.match(/.[!,@,#,$,%,^,&,*,?,_,~,-,(,),]/)) punkty++;
+    if (punkty < 1) {
+        document.getElementById("opisHasla").innerHTML = "Bardzo słabe";
+        document.getElementById("opisHasla").className = "opisHaslaBardzoSlabe";
+    }
+    else if (punkty < 2) {
+        document.getElementById("opisHasla").innerHTML = "Słabe";
+        document.getElementById("opisHasla").className = "opisHaslaSlabe";
+    }
+    else if (punkty < 3) {
+        document.getElementById("opisHasla").innerHTML = "Średnie";
+        document.getElementById("opisHasla").className = "opisHaslaSrednie";
+    }
+    else {
+        document.getElementById("opisHasla").innerHTML = "Silne";
+        document.getElementById("opisHasla").className = "opisHaslaSilne";
+    }
+}
---
+++ PAI_frysny/js/Walidacja.js
@@ -0,0 +1,70 @@
+﻿function sprawdz(form)
+{
+    if (form.imie.value == "") {
+        alert("Proszę wprowadzić imię.");
+        form.imie.focus();
+        return false;
+    }
+    if (form.email.value == "") {
+        alert("Proszę wprowadzić adres email.");
+        form.email.focus();
+        return false;
+    }
+    if (form.cenaOd.value == "") {
+        alert("Proszę wprowadzić wartość od.");
+        form.cenaOd.focus();
+        return false;
+    }
+    if (form.cenaDo.value == "") {
+        alert("Proszę wprowadzić wartość do..");
+        form.cenaDo.focus();
+        return false;
+    }
+    if (!adresEmail(form.email.value)) {
+        alert("Proszę wprowadzić poprawny adres email \n w formacie: nazwa@serwer.domena .");
+        form.email.focus();
+        return false;
+    }
+    if (!kodPocztowy(form.kodPocztowy.value)) {
+        alert("Proszę wprowadzić poprawny kod pocztowy.");
+        form.kodPocztowy.focus();
+        return false;
+    }
+    if (!sprawdzWzrost(form.wzrost.value)) {
+        alert("Proszę wprowadzić poprawny wzrost w cm. (zakres 40-250cm).");
+        form.wzrost.focus();
+        return false;
+    }
+    if (!sprawdzOdDo(form.cenaOd.value, form.cenaDo.value)) {
+        alert("Proszę wprawdzić poprawny zakres cenowy");
+        form.wzrost.focus();
+        return false;
+    }
+    return true;
+}
+function adresEmail(adres) {
+    var regex = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+\.)+[a-zA-Z0-9.-]{2,4}$/;
+    return regex.test(adres);
+}
+function kodPocztowy(kod) {
+    var regex = /^[0-9]{2}\-[0-9]{3}$/;
+    return regex.test(kod);
+}
+function liczbaCalkowita(liczba) {
+    var regex = /^[\-]{0,1}[0-9]{1,8}$/;
+    return regex.test(liczba);
+}
+function sprawdzWzrost(liczba) {
+    if (!liczbaCalkowita(liczba)) { return false; }
+    var wzrost = parseInt(liczba);
+    if ((wzrost<40) || (wzrost>250)) { return false; }
+    return true;
+}
+function sprawdzOdDo(cenaOd, cenaDo) {
+    if (!liczbaCalkowita(cenaOd)) { return false; }
+    if (!liczbaCalkowita(cenaDo)) { return false; }
+    var zakres = parseInt(cenaDo) - parseInt(cenaOd);
+    if (zakres < 0) { return false; }
+    return true;
+}
+
